72f0ee50b250e5a26bd4392587cddaf59b0104bc
Vitor Germano <vitorgermanoramos@gmail.com>
2023-04-18 20:19:41 -0300

    feat: create specification

---
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -0,0 +1,32 @@
+import { Specification } from "../model/Specification";
+import {
+    ICreateSpecificationsDTO,
+    ISpecificationsRepository,
+} from "./interfaces/ISpecificationsRepository";
+
+export class SpecificationsRepository implements ISpecificationsRepository {
+    private specifications: Specification[];
+
+    constructor() {
+        this.specifications = [];
+    }
+
+    create({ name, description }: ICreateSpecificationsDTO) {
+        const specification = new Specification();
+
+        Object.assign(specification, {
+            name,
+            description,
+            created_at: new Date(),
+        });
+
+        this.specifications.push(specification);
+    }
+
+    findByName(name: string) {
+        const specification = this.specifications.find(
+            (specification) => specification.name === name
+        );
+        return specification ?? null;
+    }
+}
