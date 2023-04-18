e7438c79be4a7ea40bc572d424903122a77d3754
Vitor Germano <vitorgermanoramos@gmail.com>
2023-04-18 19:51:29 -0300

    chore: modules based arch

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,23 +1,25 @@
 import { Router } from "express";
-import { CategoriesRepository } from "../repositories/CategoriesRepository";
-import { CreateCategoryService } from "../services/CreateCategoryService";
+import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
+import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (req, res) => {
-  const { name, description } = req.body;
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
+    const { name, description } = req.body;
+    const createCategoryService = new CreateCategoryService(
+        categoriesRepository
+    );
 
-  createCategoryService.execute({ name, description });
+    createCategoryService.execute({ name, description });
 
-  return res.status(201).send();
+    return res.status(201).send();
 });
 
 categoriesRoutes.get("/", (req, res) => {
-  const result = categoriesRepository.list();
+    const result = categoriesRepository.list();
 
-  return res.json(result);
+    return res.json(result);
 });
 
 export { categoriesRoutes };
